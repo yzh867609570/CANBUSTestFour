504ec843cd050179f2005ba5bba8f3cdf9727516
yzh <867609570@qq.com>
2019-08-15 14:51:02 +0800

    1.1

--- CANBUSTestFour/Form1.cs
+++ CANBUSTestFour/Form1.cs
@@ -197,7 +197,9 @@ namespace CANBUSTestFour
                 statusText.Text = action + " failed: " + errorText;
             }
             else
+            {
                 statusText.Text = action + " succeeded";
+            }
         }
 
     }
